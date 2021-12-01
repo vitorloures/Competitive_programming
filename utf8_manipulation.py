57136872ab46c3894890cd4105180427e5f5eb58
Vitor Loures <vitorf.loures@gmail.com>
2021-12-01 09:34:51 -0300

    Add utf-8 validation problem solution

---
+++ utf8_manipulation.py
@@ -0,0 +1,71 @@
+"""
+This is my first implementation of utf-8 validation LC problem:
+https://leetcode.com/problems/utf-8-validation/solution/
+"""
+
+
+from typing import List
+
+
+class Solution:
+    def validUtf8(self, data: List[int]) -> bool:
+        binary_data = self.transformInteger2Binary(data)
+        while len(binary_data) > 0:
+            next_byte = binary_data[0]
+            is_valid, size_seq = self.isNBytesSeq(next_byte)
+
+            if not is_valid:
+                return False
+
+            if len(binary_data) < size_seq:
+                return False
+            else:
+                seq2analyse = binary_data[:size_seq]
+                binary_data = binary_data[size_seq:]
+
+            if size_seq > 1:
+                is_valid = self.check_10_presence(size_seq, seq2analyse)
+                if not is_valid:
+                    return False
+        return True
+
+    def check_10_presence(self, size_seq: int, seq2analyse: List) -> bool:
+        for byte_pos in range(1, size_seq):
+            if seq2analyse[byte_pos][:2] != '10':
+                return False
+        return True
+
+    def isNBytesSeq(self, next_byte: str) -> tuple:
+        if next_byte[0] == '0':
+            return True, 1
+        elif next_byte[:3] == '110':
+            return True, 2
+        elif next_byte[:4] == '1110':
+            return True, 3
+        elif next_byte[:5] == '11110':
+            return True, 4
+        else:
+            return False, -1
+
+    def transformInteger2Binary(self, data: List[int]) -> List[str]:
+        binary_list = []
+        for i in data:
+            binary_i = self.getBinaryString(i)
+            binary_list.append(binary_i)
+        return binary_list
+
+    def getBinaryString(self, integer: int) -> str:
+        binary_format = ''
+        integer_to_transform = integer % 256
+
+        power_two_seq = [128, 64, 32, 16, 8, 4, 2, 1]
+        for power_two_evaluated in power_two_seq:
+            if integer_to_transform >= power_two_evaluated:
+                binary_format = binary_format + '1'
+                integer_to_transform -= power_two_evaluated
+            else:
+                binary_format += '0'
+
+        return binary_format
+
+
