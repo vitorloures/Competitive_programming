2b2e82188e328520f591b2f3fceef3ba966df1d3
Vitor Loures <vitorf.loures@gmail.com>
2022-03-20 02:53:59 -0300

    Challenge nine solution

--- kick_start_2022/round_a/challenge_nine.py
+++ kick_start_2022/round_a/challenge_nine.py
@@ -1,6 +1,3 @@
-from collections import Counter
-
-
 def main():
 
     T = int(input().split()[0])
@@ -8,19 +5,30 @@ def main():
         N = int(input().split()[0])
         comp = 9 - N % 9
         N_str = str(N)
-        possible_strings = []
-        for i in range(len(N_str)+2):
-            gen_str = N_str[:i]+str(comp)+N_str[i:]
-            possible_strings.append(gen_str)
-            if comp == 9 and i > 0:
-                gen_str = N_str[:i] + '0' + N_str[i:]
-                possible_strings.append(gen_str)
+        min_sol = float('inf')
 
-        set_int = set()
-        for string in possible_strings:
-            set_int.add(int(string))
-        sol = min(set_int)
-        print(f"Case #{test_i}: {sol}")
+        i = 0
+        if comp != 9:
+            while i < len(N_str):
+                if comp < int(N_str[i]):
+                    gen_str = N_str[:i] + str(comp) + N_str[i:]
+                    gen_int = int(gen_str)
+                    if gen_int < min_sol:
+                        min_sol = gen_int
+                    break
+                i += 1
+
+            gen_int = 10 * N + comp
+            if gen_int < min_sol:
+                min_sol = gen_int
+
+        else:
+            gen_str = N_str[:1] + '0' + N_str[1:]
+            gen_int = int(gen_str)
+            if gen_int < min_sol:
+                min_sol = gen_int
+
+        print(f"Case #{test_i}: {min_sol}")
 
 
 if __name__ == "__main__":
